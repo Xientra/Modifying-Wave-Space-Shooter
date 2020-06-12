7f591121cc725fd9e303cc5228dbf5509d2753fd
ge75dod <ge75dod@users.noreply.github.com>
2020-06-13 01:24:45 +0200

    made player into a singleton, added delegate

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,31 @@
 
 public class Player : MonoBehaviour, IDamagable
 {
+	public static Player Instance { get; private set; }
+	
 	[SerializeField]
 	private int health;
+
+	public delegate void OnLifePointsChange(int lifepoints);
+	public OnLifePointsChange onLifePointsChange;
+
+	private void Awake() 
+	{
+		if (instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			i
+		}
+	}
 
 	public bool TakeDamage(int dmg)
 	{
 		health -= dmg;
+		onLifePointsChange?.Invoke(health);
+
 		if (health <= 0)
 		{
 			Die();
