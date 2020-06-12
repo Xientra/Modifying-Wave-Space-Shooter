bccfab151a921b8b5ab7fedeb7e47b964d468c36
ge75dod <ge75dod@users.noreply.github.com>
2020-06-13 00:14:41 +0200

    added a player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag.Equals("Modification")) {
+            // TODO: insert modification into inventory here
+        }
+    }
+}
