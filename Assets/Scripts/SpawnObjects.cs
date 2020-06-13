182bf7c45e54dbad0e82563f1a3a7ce7bbf18d20
Pascal Möller <pm@pascalmoeller.de>
2020-06-14 01:12:11 +0200

    Syntax fix

--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -25,11 +25,6 @@ public class SpawnObjects : MonoBehaviour
         // Iterate over Objects
         // --------------------
         int angle = 0;
-
-        Vector3 up      = transform.forward;
-        Vector3 left    = new Vector3(-1, 0, 1);
-        Vector3 right   = new Vector3(1, 0, 1);
-
         foreach(GameObject obj in m_objects)
         {
             GameObject instance         = Instantiate(obj);
@@ -39,7 +34,7 @@ public class SpawnObjects : MonoBehaviour
             Vector3 direction = Quaternion.AngleAxis(angle, transform.forward) * transform.up;
 
             move.SetDirection(direction);
-            move.SetSpeed(Random.Range(1, 10));
+            move.SetSpeed(Random.Range(1, 2));
             angle += 360 / m_objects.Count;
         }
     }
