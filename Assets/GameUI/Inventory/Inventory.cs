03db4d1b5cd98f943a036f94a9439e18beb9e968
ge75dod <ge75dod@users.noreply.github.com>
2020-06-13 16:21:58 +0200

    cleaned up my folders, restructured the inventory

---
+++ Assets/GameUI/Inventory/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    InventorySlot[] slots = new InventorySlot[8];
+
+    [SerializeField]
+    public Color nonRemovableColor = new Color(219, 168, 57, 180); // orange
+
+    public void insertModification(Modification mod, Sprite icon) 
+    {
+        for (int i = 0; i < slots.Length; i++) {
+            if (slots[i] == null) {
+                slots[i].setContent(mod);
+                slots[i].setIcon(icon);
+
+                if (!mod.IsRemovable())
+                {
+                    slots[i].setColor(nonRemovableColor);
+                }
+
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/GameUI/Inventory/InventorySlot.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour, IPointerClickHandler
+{
+    private Modification content = null;
+    private Sprite icon = null;
+
+    private Image background;
+    [SerializeField]
+    private Color baseColor = new Color(14, 30, 46, 180);
+    [SerializeField]
+    public Color equippedColor = new Color(140, 212, 57, 180); // green
+
+    void Awake()
+    {
+        this.background = this.gameObject.GetComponent<Image>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            Debug.Log("RightClick detected");
+            if (content.IsRemovable())
+            {
+               // TODO: depending on type: remove from player or projectile
+
+               // reset slot
+               content = null;
+               icon = null;
+               background.color = baseColor;
+            }
+        }
+        /*if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            Debug.Log("LeftClick detected");
+            content.SetEquipped(true);
+            background.color = equippedColor;
+        }*/
+    }
+
+    public void setContent(Modification content) {
+        this.content = content;
+    }
+
+    public void setIcon(Sprite icon) {
+        this.icon = icon;
+    }
+
+    public void setColor(Color color) {
+        background.color = color;
+    }
+}
