dcd86f8e373f47d03025c605b06a11b8aa3fa883
ge75dod <ge75dod@users.noreply.github.com>
2020-06-14 11:11:06 +0200

    overhauled the main menu

--- Assets/MainMenu/MainMenu.cs
+++ Assets/MainMenu/MainMenu.cs
@@ -1,10 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject instructions;
+    [SerializeField]
+    private GameObject menu;
+    [SerializeField]
+    private GameObject logo;
+
     public void PlayGame()
     {
+        /*StartCoroutine("ShowInstructions");*/
         SceneManager.LoadScene(1);
     }
 
@@ -21,4 +30,18 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
+    public void ShowInstructions() 
+    {
+        menu.SetActive(false);
+        logo.SetActive(false);
+        instructions.SetActive(true);
+    }
+
+    /*IEnumerator ShowInstructions() 
+    {
+        menu.SetActive(false);
+        instructions.SetActive(true);
+        yield return new WaitForSeconds(500.0f);
+    }*/
 }
